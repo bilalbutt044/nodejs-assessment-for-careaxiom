3171e87bbcde489080a808e079cf8c5465120be1
Muhammad Bilal <bilal.soft000@gmail.com>
2024-10-26 22:08:17 +0500

    callback implementation

---
+++ callback-implementation.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const url = require('url');
+const { fetchTitle, generateHtmlResponse } = require('./util');
+
+// Define the hostname and port
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// Create the server to accept HTTP requests
+const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  const pathname = parsedUrl.pathname;
+  const query = parsedUrl.query;
+
+  if (pathname === '/I/want/title' || pathname === '/I/want/title/') {
+    if (query.address) {
+      const addresses = Array.isArray(query.address) ? query.address : [query.address];
+      let results = [];
+      let completedRequests = 0;
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+
+      addresses.forEach((address, index) => {
+        fetchTitle(address, (result) => {
+          results[index] = result;  // Store each result in the correct index
+          completedRequests += 1;
+
+          if (completedRequests === addresses.length) {
+            const htmlResponse = generateHtmlResponse(results)
+            res.end(htmlResponse);
+          }
+        });
+      });
+    } else {
+      res.statusCode = 400;
+      res.end('<html><body><h1>Please provide at least one address query parameter.</h1></body></html>');
+    }
+  } else {
+    res.statusCode = 404;
+    res.end('<html><body><h1>No route found</h1></body></html>');
+  }
+});
+
+// Start the server
+server.listen(port, hostname, () => {
+  console.log(`HTTP server running at http://${hostname}:${port}/`);
+});
---
+++ util.js
@@ -0,0 +1,73 @@
+const http = require("http");
+const https = require("https");
+const url = require("url");
+
+// Helper function to normalize a URL to HTTPS
+function normalizeUrl(address) {
+  // If the URL does not start with 'http://' or 'https://', add 'https://' as the default protocol
+  if (!/^https?:\/\//i.test(address)) {
+    return `https://${address}`;
+  }
+  return address;
+}
+
+function generateHtmlResponse(titles) {
+  return `
+              <html>
+              <head></head>
+              <body>
+                <h1>Following are the titles of given websites:</h1>
+                <ul>
+                  ${titles.join("\n")}
+                </ul>
+              </body>
+              </html>
+            `;
+}
+
+// Helper function to validate a URL format
+function isValidUrl(string) {
+  try {
+    new URL(string);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
+// Function to fetch the title from a given URL using HTTPS
+function fetchTitle(targetUrl, callback) {
+  const normalizedUrl = normalizeUrl(targetUrl);
+
+  if (!isValidUrl(normalizedUrl)) {
+    return callback(`<li>${targetUrl} - NO RESPONSE</li>`);
+  }
+
+  https.get(normalizedUrl, (res) => {
+    // Check for redirection status codes (301 or 302)
+    if (res.statusCode === 301 || res.statusCode === 302) {
+      const location = res.headers.location;
+      console.log(`Redirecting from ${normalizedUrl} to ${location}`);
+      return fetchTitle(location, callback); // Follow the redirect
+    }
+
+    let data = '';
+    res.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    res.on('end', () => {
+      const titleMatch = data.match(/<title>([^<]*)<\/title>/i);
+      if (titleMatch && titleMatch[1]) {
+        callback(`<li>${targetUrl} - "${titleMatch[1]}"</li>`);
+      } else {
+        callback(`<li>${targetUrl} - NO RESPONSE</li>`);
+      }
+    });
+  }).on('error', (err) => {
+    callback(`<li>${targetUrl} - NO RESPONSE</li>`);
+  });
+}
+
+
+module.exports = { fetchTitle, normalizeUrl, generateHtmlResponse };
